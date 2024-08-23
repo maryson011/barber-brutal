5172c3fc9199550d3564712576abdaaaead13d97
maryson gleison <maryson011@gmail.com>
2024-08-23 08:56:43 -0300

    feat: add section background component

--- barba-brutal/frontend/src/app/page.tsx
+++ barba-brutal/frontend/src/app/page.tsx
@@ -1,12 +1,19 @@
 import NossosProfissionais from "@/components/profissional/NossosProfissionais";
 import NossosServicos from "@/components/servico/NossosServicos";
+import SecaoBackground from "@/components/shared/SecaoBackground";
 
 export default function Landing() {
   return (
-    <div className="flex flex-col gap-10 container">
-      <h1 className="text-6xl">Landing page</h1>
-      <NossosServicos />
-      <NossosProfissionais />
+    <div className="flex flex-col">
+      <div className="container py-10">
+        <h1 className="text-6xl">Landing page</h1>
+      </div>
+      <SecaoBackground imagem='/banners/servicos.webp'>
+        <NossosServicos />
+      </SecaoBackground>
+      <SecaoBackground imagem="/banners/profissionais.webp">
+        <NossosProfissionais />
+      </SecaoBackground>
     </div>
   )
 }
