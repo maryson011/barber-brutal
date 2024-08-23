a5e4e1aff09828fb24635a0cc3c7e396bdfe87e2
maryson gleison <maryson011@gmail.com>
2024-08-23 17:37:40 -0300

    feat: add first post user

--- barba-brutal-x/apps/backend/src/auth/auth.controller.ts
+++ barba-brutal-x/apps/backend/src/auth/auth.controller.ts
@@ -1,9 +1,37 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PrismaService } from 'src/db/prisma.service';
+
+interface Usuario {
+    id?: number
+    nome: string
+    email: string
+    senha?: string
+    telefone?: string
+    barbeiro?: boolean
+}
 
 @Controller('auth')
 export class AuthController {
+
+    constructor(private readonly prisma: PrismaService) {
+
+    }
+
     @Post('login')
     async login() {
         return 'login'
+    }
+
+    @Post('registrar')
+    async registrar(@Body() usuario: Usuario) {
+        await this.prisma.usuario.create({
+            data: {
+                nome: usuario.nome,
+                email: usuario.email,
+                senha: usuario.senha,
+                telefone: usuario.telefone,
+                barbeiro: usuario.barbeiro ?? false,
+            }
+        })
     }
 }
