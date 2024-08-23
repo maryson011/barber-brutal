daa94e61ec405e15bf108d0af234b2cd045f8b93
maryson gleison <maryson011@gmail.com>
2024-08-23 16:46:30 -0300

    feat: add prisma and migrate user

---
+++ barba-brutal-x/apps/backend/prisma/migrations/20240823194348_criar_usuario/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "usuario" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "barbeiro" BOOLEAN NOT NULL
+);
