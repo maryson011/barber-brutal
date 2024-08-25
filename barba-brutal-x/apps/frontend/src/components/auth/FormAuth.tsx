d7682bfbf0b0e81c077f29712d28a9fa3f75d5c7
maryson gleison <maryson011@gmail.com>
2024-08-25 16:42:46 -0300

    feat: add authentication form #2

--- barba-brutal-x/apps/frontend/src/components/auth/FormAuth.tsx
+++ barba-brutal-x/apps/frontend/src/components/auth/FormAuth.tsx
@@ -1,7 +1,15 @@
+'use client'
 import Image from "next/image"
 import Logo from "../shared/Logo"
+import Link from "next/link"
+import { useState } from "react"
 
 export default function FormAuth() {
+    const [modo, setModo] = useState<'login' | 'cadastro'>('login')
+
+    function alternarModo() {
+        setModo(modo === 'login' ? 'cadastro' : 'login')
+    }
     return (
         <div className="flex justify-center items-center h-screen">
             <Image src="/banners/principal.webp" fill alt="banner" />
@@ -12,14 +20,39 @@ export default function FormAuth() {
                 "
             >
                 <Logo />
+                <div>
+                    {modo === 'login' ? (
+                        <h1 className="text-2xl font-thin">Seja bem vindo</h1>
+                    ) : (
+                        <h1 className="text-2xl font-thin">Cadastro</h1>
+                    )}
+                </div>
                 <div className="flex flex-col gap-4 w-80">
-                    <input type="text" placeholder="Nome" className="input"/>
+                    {modo === 'cadastro' && (
+                        <input type="text" placeholder="Nome" className="input"/>
+                    )}
                     <input type="text" placeholder="E-mail" className="input"/>
                     <input type="password" placeholder="Senha" className="input" />
-                    <input type="tel" placeholder="Telefone" className="input" />
+                    {modo === 'cadastro' && (
+                        <input type="tel" placeholder="Telefone" className="input" />
+                    )}
                     <div className="flex gap-2">
-                        <button className="button flex-1 bg-green-600">Cadastrar</button>
-                        <button className="button flex-1">Cancelar</button>
+                        <button className="button flex-1 bg-green-600">
+                            {modo === 'login' ? 'Entrar' : 'Cadastrar'}
+                        </button>
+                        <Link href="/" className="button flex-1 flex justify-center">Cancelar</Link>
+                    </div>
+                    <div className="flex mt-6">
+                        <button 
+                            onClick={alternarModo} 
+                            className="button-outline flex-1"
+                        >
+                            {modo === 'login' ? (
+                                <div>Ainda não tem conta? <span className="text-yellow-400 font-bold">Cadastre-se</span></div>
+                            ) : (
+                                <div>Já tem conta? <span className="text-yellow-400 font-bold">Entre na plataforma</span></div>
+                            )}
+                        </button>
                     </div>
                 </div>
                 <span>Formulário de autenticação</span>
